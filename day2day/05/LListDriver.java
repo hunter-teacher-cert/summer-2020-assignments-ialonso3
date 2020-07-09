927a432252b04540bf311eb94b0104b8d6427d78
ialonso3 <ialonso@manhattanbridgeshs.org>
2020-07-09 14:55:14 -0400

    Modified Driver for my LList

--- day2day/05/LListDriver.java
+++ day2day/05/LListDriver.java
@@ -5,6 +5,9 @@ public class LListDriver {
   public static void main(String[] args) {
 
 		LList g = new LList();
+		
+		final int FRONT = -10000;
+		final int END = -20000;
 
 		System.out.println("Test Empty list print & isEmpty:");
 		System.out.println(g);
@@ -12,9 +15,9 @@ public class LListDriver {
 		System.out.println(g.length());
 
 		System.out.println("\nTest addFront 3x & isEmpty():");
-		g.addFront("good");
-		g.addFront("news");
-		g.addFront("everyone!");
+		g.add("good", FRONT);
+		g.add("news", FRONT);
+		g.add("everyone!", FRONT);
 		System.out.println(g);
 		System.out.println(g.isEmpty());
 		System.out.println(g.length());
@@ -22,31 +25,64 @@ public class LListDriver {
 	   System.out.println("\nTest get 0, 1, 5:");
 	   System.out.println(g.get(0));
 	   System.out.println(g.get(1));
-	   System.out.println(g.get(5));
+	   try {
+		   System.out.println(g.get(5));
+	   }
+	   catch ( IndexOutOfBoundsException e ) {
+		   e.printStackTrace();
+		   System.out.println("Did not get(5)");
+	   }
 
 	   System.out.println("\nTest set 0, 1, 5:");
 	   g.set(0, "me!");
 	   g.set(1, "job");
-	   g.set(5, "wow");
+	   try {
+		   g.set(2, null);
+	   }
+	   catch ( IllegalArgumentException e ) {
+		   e.printStackTrace();
+		   System.out.println("null Not set");
+	   }
+	   try {
+		   g.set(5, "wow");
+	   }
+	   catch ( IndexOutOfBoundsException e ) {
+		   e.printStackTrace();
+		   System.out.println("5 is too far");
+	   }
 	   System.out.println(g);
 
 	   System.out.println("\nTest insert 0, 2, 5, 10:");
-	   g.insert(0, "woo!");
-	   g.insert(2, "go");
-	   g.insert(5, "cool");
-	   g.insert(10, "too far");
+	   g.add("woo!",0);
+	   g.add("go", 2);
+	   g.add("cool", 5);
+	   try {
+		   g.add("too far", 10);
+	   }
+	   catch ( IndexOutOfBoundsException e ) {
+		   e.printStackTrace();
+		   System.out.println("5 is too far");
+	   } 
 	   System.out.println(g);
 	   System.out.println(g.length());
 	   
 	   System.out.println("\nTest append");
-	   g.append("EXTRA!");
+	   g.add("EXTRA!", END);
 	   System.out.println(g);
 	   System.out.println(g.length());
 
 	   System.out.println("\nTest search \"woo!\", \"cool\", \"too far\"");
 	   System.out.println(g.search("woo!"));
 	   System.out.println(g.search("cool"));
-	   System.out.println(g.search("too far"));
+	   System.out.println(g.search("too far"));  //OK
+	   try {
+		   System.out.println(g.search(null));
+	   }
+	   catch ( IllegalArgumentException e ) {
+		   e.printStackTrace();
+		   System.out.println("null Not found");
+	   } 
+	   
 
 	   System.out.println("\nTest remove 0, 2:");
 	   g.remove(0);
