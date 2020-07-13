d8e224f1ea3a6503b388a9b6a8c0c2d44ef44858
ialonso3 <ialonso@manhattanbridgeshs.org>
2020-07-13 16:13:11 -0400

    added reverseWord & isWordPalindrome

--- day2day/09/StackApp.java
+++ day2day/09/StackApp.java
@@ -15,8 +15,37 @@ public class StackApp {
 			return str;
 	}
 	
+	public static String reverseWord(String s){
+			Stack<String> w = new Stack<String>();
+			String str = "";
+			int inx = 0;
+			int i = 0;
+			while(i < s.length()) {
+				inx = s.indexOf(' ',i);
+				if (inx > -1) {
+					if (i==0)
+						// do not add the space
+						w.push(s.substring(i,inx));
+					else
+						w.push(s.substring(i,inx)+" ");
+					i = inx + 1;
+				} else {
+					w.push( s.substring(i,s.length())+" ");
+					i = s.length();
+				}
+			}
+			while (! w.isEmpty() ) {
+				str += w.pop();
+			}
+			return str;
+	}
+	
 	public static boolean isPalindrome(String s) {
 		return s.equals( reverse(s) );
+	}
+	
+	public static boolean isWordPalindrome(String s) {
+		return s.equals( reverseWord(s) );
 	}
 	
 	public static boolean parenCheck(String s) {
@@ -37,21 +66,42 @@ public class StackApp {
 	
 	public static void main(String[] args){
 		String str = "";
+		System.out.println("***************************");
 		str = "I am what I am";
 		System.out.println("Reverse: "+str);
 		System.out.println(reverse(str));
+		System.out.println("***************************");
+		System.out.println("Word reverse: "+str);
+		System.out.println(reverseWord(str));
+		System.out.println("***************************");
 		str = "racecar";
 		System.out.println("Palindrome: "+str);
 		System.out.println( isPalindrome(str));
+		System.out.println("***************************");
 		str = "banana";
 		System.out.println("Not Palindrome: "+str);
 		System.out.println( isPalindrome(str));
+		System.out.println("***************************");
+		str = "I am what am I";
+		System.out.println("Word Palindrome: "+str);
+		System.out.println( isWordPalindrome(str));
+		System.out.println("***************************");
+		str = "I am not what am I not";
+		System.out.println("Not Word Palindrome: "+str);
+		System.out.println( isWordPalindrome(str));
+		System.out.println("***************************");
 		str = "5(3+(5-1)(5))";
 		System.out.println("Correct stmt: "+str);
 		System.out.println(parenCheck(str));
+		System.out.println("***************************");
 		str = "5(3+(5-1(5))";
 		System.out.println("Incorrect stmt: "+str);
 		System.out.println(parenCheck(str));
+		System.out.println("***************************");
+		str = "53)+(5-1)(5)(";
+		System.out.println("Incorrect stmt: "+str);
+		System.out.println(parenCheck(str));
+		System.out.println("***************************");
     }
 	
 
