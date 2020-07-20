918b486e79eb38137c1c982094f9a60c9c3d01f2
ialonso3 <ialonso@manhattanbridgeshs.org>
2020-07-20 19:39:06 -0400

    cleaned it up and better delete, minor bug still

--- day2day/14/BSTree.java
+++ day2day/14/BSTree.java
@@ -65,32 +65,24 @@ public class BSTree {
 		System.out.println();
 	}
 	
-	
-	private void getMaxLeft(TreeNode curr, ArrayList<Integer> al) {
-		if (curr == null)
-			return;
-		
-		// process left
-		getMaxLeft(curr.getLeft(),al);
-		
-		//process the current node
-		al.add(curr.getData());
-		
-		//process right
-		getMaxLeft(curr.getRight(),al);
+	private int getMaxLeft(TreeNode curr) {
+		TreeNode trailer = null;
+		while (curr != null) {
+			trailer = curr;
+			curr = curr.getRight();
+		}
+		return trailer.getData();
 	}
 	
 	// private int getMinRight(TreeNode curr) {
-		// if (curr == null)
-			// throw new NullPointerException();
-		// int min = curr.getData();
+		// TreeNode trailer = null;
 		// while (curr != null) {
-			// if (curr.getData() < min)
-				// min = curr.getData();
-			// curr = curr.getRight();
+			// trailer = curr;
+			// curr = curr.getLeft();
 		// }
-		// return min;		
+		// return trailer.getData();
 	// }
+
 	
 	// ************* DELETE **************8
 	// Z's delete skeleton
@@ -101,14 +93,9 @@ public class BSTree {
 			return;
 		}
 		
-		// find the node that we want to delete
-		// and the node above it using piggybacking
 		TreeNode front = root;
 		TreeNode trailer = root;
 
-		// do the piggyback loop
-		// until we either find the node or null
-		// if the key isn't present
 		while (front != null && front.getData() != key ){
 			if (front.getData() < key){
 				trailer = front;
@@ -125,7 +112,7 @@ public class BSTree {
 		}
 
 		// if we get here
-		// front points to the node we want to delete
+		// front points to the node we want
 		// and trailer points to the one above it
 		
 		// Is front to the Left or Right of trailer?
@@ -137,19 +124,13 @@ public class BSTree {
 		// front has two children
 		//
 		// find the node with the largest value
-		// on fronts left subtree
-		// and replace front with it.
-			// if (isLeft) {
-				ArrayList<Integer> al = new ArrayList<Integer>();
-				getMaxLeft(front.getLeft(),al);
-				int maxVal = al.get(al.size()-1);
-				delete(maxVal);
-				front.setValue(maxVal);
-			// } else {
-				// int minVal = getMinRight(front.getRight());		
-				// delete(minVal);
-				// front.setValue(minVal);
-			// }
+		// on front's left subtree & replace front with it.
+			int maxVal = getMaxLeft(front.getLeft());
+			delete(maxVal);
+			front.setValue(maxVal);
+			// int minVal = getMinRight(front.getRight());
+			// delete(minVal);
+			// front.setValue(minVal);
 		} else {
 			// front has one or no children
 			TreeNode setNode = null;
@@ -168,95 +149,6 @@ public class BSTree {
 				trailer.setRight(setNode);
 		}
 	}
-	
-	/********************************************
-	// use the transverse to insert all the nodes
-	// in a given orphaned tree
-	private void inorderInsert(TreeNode current) {
-		if (current == null)
-			return;	
-		// process left
-		inorderTraverse(current.getLeft());	
-		//process the current node
-		insert(current.getData());
-		//process right
-		inorderTraverse(current.getRight());	
-	}
-
-	public void delete(int key) {
-		TreeNode front = root;
-		TreeNode trailer = null;
-		while (front != null){
-			int frontVal = front.getData();
-			if (frontVal == key){  // found it
-				if (trailer != null) {
-					// front is to the left
-					if(trailer.getLeft().getData()==frontVal) {
-						// does front have both left and right?
-						if (front.getRight()!=null && front.getLeft()!=null){
-							// trailer left points to front's left
-							trailer.setLeft(front.getLeft());
-							// but front's right side is orphaned
-							// Insert all the Nodes on front's right
-							this.inorderInsert(front.getRight());				
-						} else if (front.getRight() != null) {
-						// front has only a right
-							// point trailer's left to front's right
-							trailer.setLeft(front.getRight());
-							return;
-						} else if (front.getLeft() != null) {
-						// front has only a left
-							// point trailer's left to front's left
-							trailer.setLeft(front.getLeft());
-							return;
-						} else {
-						// both left and right of front are null
-							// just set trailer's left to null
-							trailer.setLeft(null);
-							return;
-						}	
-					} else {
-					// front is to the right
-						// does front have both left and right?
-						if (front.getRight()!=null && front.getLeft()!=null){
-							// trailer right points to front's right
-							trailer.setRight(front.getRight());
-							// but front's left side is orphaned
-							// Insert all the Nodes on front's left
-							this.inorderInsert(front.getLeft());				
-						} else if (front.getRight() != null) {
-						front has only a right
-							point trailer's right to front's right
-							trailer.setRight(front.getRight());
-							return;
-						} else if (front.getLeft() != null) {
-						front has only a left
-							point trailer's right to front's left
-							trailer.setRight(front.getLeft());
-							return;
-						} else {
-						both left and right of front are null
-							just set trailer's left to null
-							trailer.setRight(null);
-							return;
-						}
-					}
-				} else {
-				// key is at the root
-					front.setLeft(root.getLeft());
-					this.inorderInsert(front.getRight());
-				}
-			} else if (frontVal < key){
-				trailer = front;
-				front = front.getRight();
-			} else {
-				trailer = front;
-				front = front.getLeft();
-			}
-		}
-		throw new NullPointerException();
-	}
-	// ********************************/
 	
 	public void insert(int key) {
 		TreeNode newNode = new TreeNode(key);
@@ -285,27 +177,6 @@ public class BSTree {
 		else
 			trailer.setLeft(newNode);
 	}
-	
-	// finds the Node with that key &
-	// returns the previous one
-	// private TreeNode getPrevious(int key)
-	// {
-		// TreeNode front = root;
-		// TreeNode trailer = null;
-		// while (front != null){
-			// int frontVal = front.getData();
-			// if (frontVal == key){
-				// return trailer;
-			// } else if (frontVal < key){
-				// trailer = front;
-				// front = front.getRight();
-			// } else {
-				// trailer = front;
-				// front = front.getLeft();
-			// }
-		// }
-		// return trailer;
-	// }
 	
 	public int search(int key){
 		TreeNode curr = root;
@@ -341,6 +212,4 @@ public class BSTree {
 		root.getRight().setRight(t);
 	
 	}
-    
-    
 }
