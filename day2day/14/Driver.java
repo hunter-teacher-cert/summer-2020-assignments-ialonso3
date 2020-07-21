49a4ed9b8e35994a1540c30a85865135a21a8f3f
ialonso3 <ialonso@manhattanbridgeshs.org>
2020-07-21 10:34:02 -0400

    better testing

--- day2day/14/Driver.java
+++ day2day/14/Driver.java
@@ -22,30 +22,44 @@ public class Driver {
 		//*/
 		
 		t.insert(10);
+		t.inorderTraverse();
 		t.insert(20);
+		t.inorderTraverse();
 		t.insert(5);
+		t.inorderTraverse();
 		t.insert(7);
+		t.inorderTraverse();
 		t.insert(8);
+		t.inorderTraverse();
 		t.insert(3);
-		
-		t.preorderTraverse();
-		t.postorderTraverse();
 		t.inorderTraverse();
 		
+		// t.preorderTraverse();
+		// t.postorderTraverse();
+		// t.inorderTraverse();
+		
+		System.out.println("*********Delete 5*******");
 		t.delete(5);
 		t.inorderTraverse();
+		System.out.println("*********Insert 25*******");
 		t.insert(25);
 		t.inorderTraverse();
+		System.out.println("*********Delete 7*******");
 		t.delete(7);
 		t.inorderTraverse();
+		System.out.println("*********Delete 10*******");
 		t.delete(10);
 		t.inorderTraverse();
+		System.out.println("*********Delete 25*******");
 		t.delete(25);
 		t.inorderTraverse();
+		System.out.println("*********Delete 3*******");
 		t.delete(3);
 		t.inorderTraverse();
+		System.out.println("*********Delete 8*******");
 		t.delete(8);
 		t.inorderTraverse();
+		System.out.println("*********Delete 20*******");
 		t.delete(20);
 		t.inorderTraverse();
 		
